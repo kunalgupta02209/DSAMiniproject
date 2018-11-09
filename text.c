343afad2d20affd4ac8f429969c1dae43cf9293f
Kunal Gupta <kunallguptaa@gmail.com>
2018-11-09 18:24:53 +0530

    Added display function and fixed other bugs

--- text.c
+++ text.c
@@ -31,68 +31,7 @@ void convert_str_to_words(char *str, Word *wFront, Word *wEnd, int *chars, int *
 }
 */
 
-Line* convert_str_to_chs(char *str)
-{
-	Line *l = (Line*)malloc(sizeof(Line*));
-	l->no_of_chars = strlen(str);
-	Ch *temp, *prev;
-	for(int i = 0; i < *chars; i++)
-	{
-		temp = (Ch*)malloc(sizeof(Ch*));
-		temp->ch = str[i];
-		if(i == 0)
-		{
-			l->first_char = temp;
-			prev = temp;
-		}
-		else
-		{
-			temp->prev = prev;
-			prev->next = temp;
-			prev = temp;
-		}
-	}
-	l->last_char = prev;
-	return l;
-}
-
-Ch* findPos(Line *line, int pos)
-{
-	Ch *temp = line->first_char;
-	int i = 0;
-	while(i!=pos)
-		temp=temp->next;
-	return temp;
-}
-
-int insert_at_pos(Line *l, int position, char *str)
-{
-	if(position > l->no_of_chars+1)
-		return 0;
-	Ch *pos = findPos(l, position);	
-	Ch *pos2;
-	Line *temp;
-	if(pos != NULL)
-		pos2 = pos->next;
-	int chars = 0;
-	l->no_of_chars += chars;
-
-	if(pos != NULL)
-		pos->next = cFront;
-	else if(l->first_char == NULL)
-		l->first_char = cFront;
-	cFront->prev = pos;
-
-	if(pos2 != NULL)
-		pos2->prev = cEnd;
-	else
-		l->last_char = cEnd;
-	cEnd->next = pos2;
-
-	return 1;
-}
 /*
-
 int insert(Lines *lines, char *str)
 {
 	Line *line = (Line*)malloc(sizeof(Line*));
@@ -100,9 +39,7 @@ int insert(Lines *lines, char *str)
 	line->words = 0;
 	Word *temp, *prev;
 	int init_size = strlen(str);
-
 	char *ptr = strtok(str, " ");
-
 	while(ptr != NULL)
 	{
 		printf("'%s'\n", ptr);
@@ -125,9 +62,7 @@ int insert(Lines *lines, char *str)
 		ptr = strtok(NULL, delim);
 	}
 	if(lines->first_line == NULL)
-
 }
-
 void addLine(Lines *l, Line *ll)
 {
 	if(l->first_line == NULL)
@@ -137,13 +72,90 @@ void addLine(Lines *l, Line *ll)
 		ll->prev = (l->last_line)
 	}
 }
-
 int insert_at_pos(Lines *lines, Line *line, Word *word, char *st)
 {
-
 }
  */
 
+
+Line* convert_str_to_chs(char *str)
+{
+	Line *l = (Line*)malloc(sizeof(Line*));
+	l->no_of_chars = strlen(str);
+	Ch *temp, *prev;
+	for(int i = 0; i < l->no_of_chars; i++)
+	{
+		temp = (Ch*)malloc(sizeof(Ch*));
+		temp->ch = str[i];
+		if(i == 0)
+		{
+			l->first_char = temp;
+			prev = temp;
+		}
+		else
+		{
+			temp->prev = prev;
+			prev->next = temp;
+			prev = temp;
+		}
+	}
+	l->last_char = prev;
+	return l;
+}
+
+Ch* findPos(Line *line, int pos)
+{
+	Ch *temp = line->first_char;
+	int i = 1;
+	while(i<pos)
+	{
+		temp=temp->next;
+		i++;
+	}
+	return temp;
+}
+
+Line* insert_at_pos(Line *l, int position, char *str)
+{
+	if(position > l->no_of_chars+1)
+		return l;
+	Ch *pos = findPos(l, position);	
+	Ch *pos2;
+	Line *temp;
+	if(pos != NULL)
+		pos2 = pos->next;
+	int chars = 0;
+	temp = convert_str_to_chs(str);
+	
+	l->no_of_chars += chars;
+	if(l->first_char == NULL)
+	{
+		l = temp;
+		return l;
+	}
+	if(pos != NULL)
+		pos->next = temp->first_char;
+	(temp->first_char)->prev = pos;
+
+	if(pos2 != NULL)
+		pos2->prev = temp->last_char;
+	else
+		l->last_char = temp->last_char;
+	(temp->last_char)->next = pos2;
+
+	return l;
+}
+
+void display(Line *l)
+{
+	Ch *cur = l->first_char;
+	while(cur != NULL)
+	{
+		printf("%c", cur->ch);
+		cur = cur->next;
+	}
+	printf("\n");
+}
 
 int main()
 {
@@ -157,14 +169,33 @@ int main()
 	char str[] = "strtok needs to be called several times to split a string";
 	Line *l = (Line*)malloc(sizeof(Line));
 	l->no_of_chars = 0;
-	insert_at_pos(l,0,str);
-	//insert(lines, str);
-	//Lines lines = (Lines*)malloc(sizeof(Lines*));
-	//lines.no_of_lines = 0;
-	//lines.line_head = null;
-	//word *new = (word*)malloc(sizeof(word*));
-	//new->chs = (char*)malloc(10*sizeof(char));
-	//new->chs = "thisStr";
-	//printf("%s",new->chs);
+	l = insert_at_pos(l,1,str);
+	int choice = 1, pos = 0;
+	while(choice != 4)
+	{
+		char s[1000];
+		//display(l);
+		scanf("%d", &choice);
+		switch(choice)
+		{
+			case 1:
+				printf("Enter the text\n");
+				scanf("%s",s);
+				printf("Enter the position for insertion\n");
+				scanf("%d", &pos);
+				l = insert_at_pos(l,pos,s);
+				display(l);
+				break;
+			case 2:
+				break;
+			case 3: 
+				display(l);
+			case 4:
+				break;
+			default:
+				printf("Wrong Input selected\n");
+		}
+	}
+	
 	return 0;
 }
